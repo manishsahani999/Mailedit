66b736e7a271012e14e168099708322278568b0a
Manish sahani <manishsahani1351111@gmail.com>
2018-06-09 07:30:24 +0530

    min

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,27 @@
             <a href="{{ route('brand.create') }}" class="button is-primary">Add Brand</a>
         </div>
     </div>
+    <div class="main-body m-t-1">
+        <table class="table is-fullwidth">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Brands</th>
+                <th>lorem</th>
+                <th>edit</th>
+                <th>Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th>1</th>
+                <td><a href="">Lorem</a></td>
+                <td>23</td>
+                <td><a href="" class="button">Edit</a></td>
+                <td><a href="" class="button">Delete</a></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
