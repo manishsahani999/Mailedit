bafc39e41f49519aae9efca5e0abed5181518241
Manish sahani <manishsahani1351111@gmail.com>
2018-06-21 14:56:06 +0530

    UI changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,40 @@
-@extends('layouts.app')
-
-@section('content')
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mailedit | Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/components/login.css') }}" />
+</head>
+<body>
     <div class="wrap">
-        <div class="login-heading text-center">
-            <h2>
-                Mailed it
-                <a href="{{ route('login') }}">
-                    <span class="icon has-text-primary">
-                     <i class="fas fa-chevron-right"></i>
-                    </span>
-                </a>
-            </h2>
+        <div class="row">
+        <div class="col-sm-3 text-center" id="login-left">
+            <div>
+                <img class="m-t-2" id="mailman" src="{{ asset('img/postman.svg') }}" alt="">
+            </div>
+            <div class="p-1">
+                <h2>Log In</h2>
+            </div>
+            <div class="login_wrap">
+
+            </div>
         </div>
-        <div class="login-body">
-            <form action="{{ route('login') }}" method="post">
-                @csrf
-                <div class="form-group">
-                    <input type="text" class="form-control" name="email"
-                           value="{{ old('email') }}" required autofocus
-                           placeholder="Username or Email">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="password"
-                           value="{{ old('password') }}" required autofocus
-                           placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <div class="control">
-                        <button type="submit" class="btn btn-primary login-btn">Login</button>
-                    </div>
-                </div>
-            </form>
-            @if ($errors->any())
-                <div class="has-text-danger login-errors" style="margin-left: 0;">
-                    <ul style="list-style-type: none">
-                        @foreach ($errors->all() as $error)
-                            <li style="margin-left: -1.5em">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+        <div class="col" id="login-right">
+            <div class="text-center">
+                <img class="m-t-5" id="mail-big" src="{{ asset('img/mail_1.svg') }}" alt="">
+            </div>
+            <div class="text-center">
+                <h1>Mailedit</h1>
+            </div>
         </div>
     </div>
-@endsection
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+</body>
+</html>
