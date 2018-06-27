58e61520ea305fcc5838b75c5d6f698555e73507
Manish sahani <manishsahani1351111@gmail.com>
2018-06-27 16:45:39 +0530

    min

--- config/settings.php
+++ config/settings.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'limit' => 1,
-    'jobDelayTime' => 1,
+    'limit' => 100,
+    'jobDelayTime' => 4,
     'app' => [
         'frontend_host_url' => env('SITE_URL', 'https://messmerising.com'),
         'frontend_host_url_admin' => env('ADMIN_URL', 'http://messmerising.com'),
