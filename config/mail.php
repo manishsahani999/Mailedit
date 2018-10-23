49f4db2449065738c66606fa1d485aa4c02ff13d
Manish sahani <manishsahani1351111@gmail.com>
2018-10-23 16:02:41 +0530

    mail new configs added

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'email-smtp.us-east-1.amazonaws.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'digital@marketingdrops.com'),
+        'name' => env('MAIL_FROM_NAME', 'Mailedit'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'AKIAIRPUC7H5GF4Y3MJQ'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD', 'Ah8Nret26m3TcHX2mL6UdD1MCj1PvpNu3nv41ES/ZaFT'),
 
     /*
     |--------------------------------------------------------------------------
