6401ca7f5e230cf74efe8cbda474827eea63caef
Manish sahani <manishsahani1351111@gmail.com>
2018-10-23 16:07:51 +0530

    min

--- config/settings.php
+++ config/settings.php
@@ -4,8 +4,8 @@ return [
     'limit' => 100,
     'jobDelayTime' => 4,
     'app' => [
-        'frontend_host_url' => env('SITE_URL', 'https://messmerising.com'),
-        'frontend_host_url_admin' => env('ADMIN_URL', 'http://messmerising.com'),
+        'frontend_host_url' => env('SITE_URL', 'http://binarymail.herokuapp.com'),
+        'frontend_host_url_admin' => env('ADMIN_URL', 'http://binarymail.herokuapp.com'),
         'test_tracking_token' => '447d2c8dc25efbc493788a322f1a00e7'
     ]
 ]
