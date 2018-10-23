b8d08b51f7c6d80ada22744383e766def02a7c8d
Manish sahani <manishsahani1351111@gmail.com>
2018-10-23 15:45:20 +0530

    DB config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'keep_writing'),
+            'username' => env('DB_USERNAME', 'mani_sh999'),
+            'password' => env('DB_PASSWORD', 'manish.s19'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
