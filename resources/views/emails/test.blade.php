17c75d046c8f09bb8e741d930ea5d6dcf8544e5a
Manish sahani <manishsahani1351111@gmail.com>
2018-11-03 17:38:25 +0530

    min

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -3,6 +3,5 @@
 {!! $content !!}
 {{-- <img src="http://localhost:8000/email-open-tracking/7eb869e6-f854-44c8-92d5-4515cdb84272" alt=""> --}}
 @if (isset($links))
-    <a href="{{ $links['openTrackingLink'] }}">test image</a>
     <img src="{{ $links['openTrackingLink'] }}">
 @endif
