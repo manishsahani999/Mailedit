158457950d9c9705312c5d7db3b8dce3f9f61632
Manish sahani <manishsahani1351111@gmail.com>
2018-11-07 22:26:41 +0530

    mail env

--- app/Mail/SubMail.php
+++ app/Mail/SubMail.php
@@ -30,6 +30,6 @@ class SubMail extends Mailable implements ShouldQueue
      */
     public function build()
     {
-        return $this->subject('Thankyou for subscribing to our monthly newsletter')->view('emails.subs');
+        return $this->subject('Thank You for subscribing to our monthly newsletter')->view('emails.subs');
     }
 }
