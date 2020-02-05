eda554c2c727a50e4d8daf9f135af3251f314a35
Manish Sahani <manishsahani1351111@gmail.com>
2020-02-05 23:41:17 +0530

    all done

--- resources/views/pages/campaigns/stats.blade.php
+++ resources/views/pages/campaigns/stats.blade.php
@@ -1,174 +1,193 @@
 @extends('layouts.app')
 
 @section('content')
-    <?php
-            $sent = count($campaign->emails->where('status','sent'));
-            $opened_rate = ($opened / $sent)*100;
-            $total_clicked = $sent*count($links);
-            if ($total_clicked == 0) {
-                $clicked_rate = 0;
-            }
-            else {
-                $clicked_rate = ( $clicked / $total_clicked )*100;
-            }
-    ?>
-    <div class="wrap">
-        <div class="home-header">
-            <h2 class="inline-pc">{{ ucwords($brand->brand_name) }}</h2>
-            <h4 class="inline-pc">/ {{ $campaign->name }} / Stats</h4>
-            <div class="mt-1"><span>lorem ipsum dolo sit.</span></div>
-        </div>
-        <div class="home-body">
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="pl-3">
-                        <h2 class="t-lg inline-pc">{{ ucwords($campaign->status) }}</h2>
-                        @if ($campaign->status == 'sent')
-                            <span class="ml-1 t-lg"><strong>to</strong></span> 
-                            <h2 class="ml-1 inline-pc">{{ $campaign->recipients_count }}</h2>
-                        @endif
-                        @if ($campaign->status == 'sending')
-                            <span class="ml-1 t-lg"><strong>to</strong></span> 
-                            <h2 class="ml-1 inline-pc">{{ $campaign->recipients_count }}</h2>
-                        @endif
-                    </div>
-                    <div class="pl-3 pr-3 pt-3">
-                        <div class="mb-2">
-                            <p class="inline-pc t-lg"><strong>Recipients </strong></p>
-                            <h4 class="mb-0 inline-pc">{{ $campaign->recipients_count }}</h4>
-                        </div>
-                        <div class="mb-2">
-                            <p class="inline-pc t-lg"><strong>Ended at</strong></p>
-                            <h6 class="inline-pc">{{ (isset($campaign->starts_at)) ? $campaign->starts_at : 'Not Started'  }}</h6>
-                        </div>
-                        <div class="mb-2">
-                            <p class="inline-pc t-lg"><strong>Ended at</strong></p>
-                            <h6 class="inline-pc">{{ (isset($campaign->ends_at)) ? $campaign->ends_at : 'Not ended yet'  }}</h6>
-                        </div>
-                        <div class="mb-3">
-                            
-                        </div>
-                    </div>
-                    <hr class="mr-5">
+@if (count($campaign->emails->where('status','sent')) > 0)
+<?php
+$sent = count($campaign->emails->where('status', 'sent'));
+if ($sent == 0) $sent = 1;
+$opened_rate = ($opened / $sent) * 100;
+
+$total_clicked = $sent * count($links);
+if ($total_clicked == 0) {
+    $clicked_rate = 0;
+} else {
+    $clicked_rate = ($clicked / $total_clicked) * 100;
+}
+?>
+<div class="wrap">
+    <div class="home-header">
+        <h2 class="inline-pc">{{ ucwords($brand->brand_name) }}</h2>
+        <h4 class="inline-pc">/ {{ $campaign->name }} / Stats</h4>
+        <div class="mt-1"><span>lorem ipsum dolo sit.</span></div>
+    </div>
+    <div class="home-body">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="pl-3">
+                    <h2 class="t-lg inline-pc">{{ ucwords($campaign->status) }}</h2>
+                    @if ($campaign->status == 'sent')
+                    <span class="ml-1 t-lg"><strong>to</strong></span>
+                    <h2 class="ml-1 inline-pc">{{ $campaign->recipients_count }}</h2>
+                    @endif
+                    @if ($campaign->status == 'sending')
+                    <span class="ml-1 t-lg"><strong>to</strong></span>
+                    <h2 class="ml-1 inline-pc">{{ $campaign->recipients_count }}</h2>
+                    @endif
                 </div>
-                <div class="col">
-                    <span id="body-tab">Campaign Data</span>
-                    <hr class="mt-0"> 
-                    <div class="row">
-                        <div class="col-sm-3 pt-5 ">
-                            <h4 class=" ml-4 mt-1">Open rate</h4>
-                        </div>
-                        <div class="col-sm-9">
-                            <div class="range-slider">
+                <div class="pl-3 pr-3 pt-3">
+                    <div class="mb-2">
+                        <p class="inline-pc t-lg"><strong>Recipients </strong></p>
+                        <h4 class="mb-0 inline-pc">{{ $campaign->recipients_count }}</h4>
+                    </div>
+                    <div class="mb-2">
+                        <p class="inline-pc t-lg"><strong>Ended at</strong></p>
+                        <h6 class="inline-pc">{{ (isset($campaign->starts_at)) ? $campaign->starts_at : 'Not Started'  }}</h6>
+                    </div>
+                    <div class="mb-2">
+                        <p class="inline-pc t-lg"><strong>Ended at</strong></p>
+                        <h6 class="inline-pc">{{ (isset($campaign->ends_at)) ? $campaign->ends_at : 'Not ended yet'  }}</h6>
+                    </div>
+                    <div class="mb-3">
+
+                    </div>
+                </div>
+                <hr class="mr-5">
+            </div>
+            <div class="col">
+                <span id="body-tab">Campaign Data</span>
+                <hr class="mt-0">
+                <div class="row">
+                    <div class="col-sm-3 pt-5 ">
+                        <h4 class=" ml-4 mt-1">Open rate</h4>
+                    </div>
+                    <div class="col-sm-9">
+                        <div class="range-slider">
                             <input class="r-slider-input" type="range" value="{{ $opened_rate }}" min="0" max="100" disabled>
                             <span class="r-slider-value">{{ (int)$opened_rate }} %</span>
-                            </div>
                         </div>
-                    </div> 
-                    <div class="row">
-                        <div class="col-sm-3 pt-5 pb-5">
-                            <h4 class=" ml-4 mt-1">Click rate</h4>
-                        </div>
-                        <div class="col-sm-9">
-                            <div class="range-slider">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3 pt-5 pb-5">
+                        <h4 class=" ml-4 mt-1">Click rate</h4>
+                    </div>
+                    <div class="col-sm-9">
+                        <div class="range-slider">
                             <input class="r-slider-input" type="range" value="{{ $clicked_rate }}" min="0" max="100" disabled>
                             <span class="r-slider-value">{{ (int)$clicked_rate }} %</span>
-                            </div>
                         </div>
-                    </div> 
-                    
-                    <span id="body-tab">Raw Data</span>
-                    <hr class="mt-0">
-                    <div class="n-table-wrap">
-                        <div class="n-table row">
-                            <div class="col-sm-1 n-col-1">
-                                <img class="n-col-icon" src="{{ asset('img/trolley.svg') }}" alt="">
-                            </div>
-                            <div class="col-sm-4 n-col-2 pt-3 ">
-                                <h5>
-                                    Emails Opened
-                                </h5>
-                            </div>
-                            <div class="col-sm-2 pt-2 text-center">
-                                <h3 class="t-l mb-0">{{ $opened }}</h3>
-                                <span>Opened</span>
-                            </div>
-                            
-                            <div class="col-sm-2 pt-2 text-center">
-                                <h3 class="mb-0">{{ $sent }}</h3>
-                                <span>Sent</span>
-                            </div>
-                            <div class="col-sm-1 pt-3">
-                                <a id=""  class="btn n-lb-draft bt">View Something</a>
-                            </div>  
-                        </div>
-                        <hr>
                     </div>
-                    <div class="n-table-wrap">
-                        <div class="n-table row">
-                            <div class="col-sm-1 n-col-1">
-                                <img class="n-col-icon" src="{{ asset('img/ppc.svg') }}" alt="">
+                </div>
+
+                <span id="body-tab">Raw Data</span>
+                <hr class="mt-0">
+                <div class="n-table-wrap">
+                    <div class="n-table row">
+                        <div class="col-sm-1 n-col-1">
+                            <img class="n-col-icon" src="{{ asset('img/trolley.svg') }}" alt="">
+                        </div>
+                        <div class="col-sm-4 n-col-2 pt-3 ">
+                            <h5>
+                                Emails Opened
+                            </h5>
+                        </div>
+                        <div class="col-sm-2 pt-2 text-center">
+                            <h3 class="t-l mb-0">{{ $opened }}</h3>
+                            <span>Opened</span>
+                        </div>
+
+                        <div class="col-sm-2 pt-2 text-center">
+                            <h3 class="mb-0">{{ $sent }}</h3>
+                            <span>Sent</span>
+                        </div>
+                        <div class="col-sm-1 pt-3">
+                            <a id="" class="btn n-lb-draft bt">View Something</a>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+                <div class="n-table-wrap">
+                    <div class="n-table row">
+                        <div class="col-sm-1 n-col-1">
+                            <img class="n-col-icon" src="{{ asset('img/ppc.svg') }}" alt="">
+                        </div>
+                        <div class="col-sm-4 n-col-2 pt-3 ">
+                            <h5>
+                                Links Clicked
+                            </h5>
+                        </div>
+                        <div class="col-sm-2 pt-2 text-center">
+                            <h2 class="mb-0">{{ $clicked }}</h2>
+                            <span>Clicked</span>
+                        </div>
+                        <div class="col-sm-2 pt-2 text-center">
+                            <h3 class="mb-0">{{ $total_clicked }}</h3>
+                            <span>Total Links</span>
+                        </div>
+                        <div class="col-sm-2 pt-3">
+                            <a class="btn n-lb-draft bt" id="n-table-row-toggle">View Detailed Analysis</a>
+                        </div>
+                    </div>
+                    @if ($links)
+                    <hr id="n-table-hr" class="d-none">
+                    <div class="n-table-has-content d-none pt-1 pl-5" id="n-table-row-extended">
+                        <h5 class="mb-1">Email links</h5>
+                        <hr class="mt-0 mr-5">
+                        @foreach ($links as $link)
+                        <div class="row">
+                            <div class="col-sm-1 pt-3 text-center">
+                                <i class="fas fa-link"></i>
                             </div>
-                            <div class="col-sm-4 n-col-2 pt-3 ">
-                                <h5>
-                                    Links Clicked
-                                </h5>
+                            <div class="col-sm-3">
+                                <span>{{ (strlen($link) > 40 ) ? substr($link, 0, 40) : $link }}</span>
+                                @if (strlen($link) > 40)
+                                <span>........</span><span class="n-lb n-lb-draft">more</span>
+                                @endif
                             </div>
-                            <div class="col-sm-2 pt-2 text-center">
-                                <h2 class="mb-0">{{ $clicked }}</h2>
+                            <div class="col-sm-1 offset-sm-1 pt-3 text-center">
+                                <a target="_blank" href="{{ $link }}"><i class="fas fa-external-link-alt"></i></a>
+                            </div>
+                            <div class="col-sm-1 offset-sm-1 text-center">
+                                <h4 class="mb-0">8</h4>
                                 <span>Clicked</span>
                             </div>
-                            <div class="col-sm-2 pt-2 text-center">
-                                <h3 class="mb-0">{{ $total_clicked }}</h3>
-                                <span>Total Links</span>
+                            <div class="col-sm-1 offset-sm-1 text-center">
+                                <h4 class="mb-0">8 <span class="t-lg">%</span></h4>
+                                <span>Percentage</span>
                             </div>
-                            <div class="col-sm-2 pt-3">
-                                <a class="btn n-lb-draft bt" id="n-table-row-toggle">View Detailed Analysis</a>
-                            </div>  
                         </div>
-                        @if ($links)
-                        <hr id="n-table-hr" class="d-none">
-                        <div class="n-table-has-content d-none pt-1 pl-5" id="n-table-row-extended">
-                            <h5 class="mb-1">Email links</h5>    
-                            <hr class="mt-0 mr-5">
-                            @foreach ($links as $link)
-                                <div class="row">
-                                    <div class="col-sm-1 pt-3 text-center">
-                                        <i class="fas fa-link"></i>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <span>{{ (strlen($link) > 40 ) ? substr($link, 0, 40) : $link }}</span>
-                                        @if (strlen($link) > 40)
-                                            <span>........</span><span class="n-lb n-lb-draft">more</span>
-                                        @endif
-                                    </div>
-                                    <div class="col-sm-1 offset-sm-1 pt-3 text-center">
-                                        <a target="_blank" href="{{ $link }}"><i class="fas fa-external-link-alt"></i></a>
-                                    </div>
-                                    <div class="col-sm-1 offset-sm-1 text-center">
-                                        <h4 class="mb-0">8</h4>
-                                        <span>Clicked</span>
-                                    </div>
-                                    <div class="col-sm-1 offset-sm-1 text-center">
-                                        <h4 class="mb-0">8 <span class="t-lg">%</span></h4>
-                                        <span>Percentage</span>
-                                    </div>
-                                </div>
-                                <hr class="mb-5 mr-5">
-                            @endforeach
-                        </div>
-                        @endif
-                        <hr> 
+                        <hr class="mb-5 mr-5">
+                        @endforeach
                     </div>
-                </div> 
+                    @endif
+                    <hr>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
+@else
+
+<section class="cover space--md pb-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <h3 class="mb-0" style="font-size: 3rem;">Update {{ $list->name }}</h3>
+                <h3 class="type--fade">You can send campaigns by creating a brand.</h3>
+                <!-- <a class="" href="{{ route('brand.create') }}">
+                    <span class="btn__text text-white">Create New Brand</span>
+                </a> -->
+            </div>
+        </div>
+    </div>
+</section>
+
+@endif
 @endsection @section('scripts')
-    <script>
-    $(document).ready(function(){
-        $("#n-table-row-toggle").click(function(){
+<script>
+    $(document).ready(function() {
+        $("#n-table-row-toggle").click(function() {
             $("#n-table-row-toggle").toggleClass("n-btn");
             $("#n-table-hr").toggleClass("d-none");
             $(".n-table-has-content").toggleClass("d-none", 4000, "easeInQuad");
--- resources/views/subs/lists/edit.blade.php
+++ resources/views/subs/lists/edit.blade.php
@@ -1,39 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="wrap">
-        <div class="login-heading m-t-2">
-            <h4 class="title text-center">
-                {{ $list->name }}
-                <span class="icon has-text-primary">
-                     <i class="fas fa-chevron-right"></i>
-                </span>
-                edit
-            </h4>
-        </div>
-        <div class="login-body">
-            <form action="{{ route('subs.list.update', $list->uuid) }}" method="post">
-                @csrf
-                @method('PUT')
-                <div class="form-group">
-                        <input type="text" value="{{ $list->name }}" name="name"
-                               class="form-control" placeholder="Name" required>
-                </div>
-                <div class="field">
-                    <div class="control">
-                        <button class="btn login-btn btn-primary    " type="submit">Update</button>
-                    </div>
-                </div>
-            </form>
-            @if ($errors->any())
-                <div class="has-text-danger login-errors" style="margin-left: 0;">
-                    <ul style="list-style-type: none">
-                        @foreach ($errors->all() as $error)
-                            <li style="margin-left: -1.5em">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+
+<section class="cover space--md pb-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <h3 class="mb-0" style="font-size: 3rem;">Update {{ $list->name }}</h3>
+                <h3 class="type--fade">You can send campaigns by creating a brand.</h3>
+                <!-- <a class="" href="{{ route('brand.create') }}">
+                    <span class="btn__text text-white">Create New Brand</span>
+                </a> -->
+            </div>
         </div>
     </div>
+</section>
+
+<section class="cover space--md pb-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form action="{{ route('subs.list.update', $list->uuid) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <input type="text" value="{{ $list->name }}" name="name" class="form-control" placeholder="Name" required>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button class="btn type--uppercase" type="submit">Update</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+</section>
 @endsection
